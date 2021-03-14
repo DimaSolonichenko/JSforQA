873789128d8367f1f3e9fa7c9e08133da968f465
dsolo <dsolonichenko@gmail.com>
2021-03-14 14:48:53 +0200

    update

--- HomeWork2-1/index.js
+++ HomeWork2-1/index.js
@@ -5,7 +5,7 @@ let b = prompt("Insert b = ");
 
 let result = (a + b < 10)? "few": "many";
 
-alert("You have " + result + " money");
+alert(`You have ${result} money`);
 
 
 //Task 2
@@ -20,7 +20,7 @@ alert(result)
 
 //Task 3
 
-const currentYear = 2021;
+const currentYear = new Date().getFullYear();
 
 let Name = prompt("What's your name?");
 alert("Hello, " + Name + "\nNice to meet you!");
@@ -28,13 +28,12 @@ alert("Hello, " + Name + "\nNice to meet you!");
 let year = prompt("Enter your  birthday year, please");
 let result = currentYear - year;
 
-alert("You are " + result + " years old" + "\nIf you already had Birthday in " + currentYear + "))");
+alert(`You are ${result} years old\nIf you already had Birthday in ${currentYear}))`);
 
 //Task 4
 
 let score = Number(prompt("Please, input your score"));
 let resultScore;
-console.log(score < 60)
 if (score >=0 && score < 60)
     resultScore = "Your mark is F";
     else if (score < 70)
@@ -49,3 +48,27 @@ if (score >=0 && score < 60)
         resultScore = "Lier!!! It's incorrect value";
 
 alert(resultScore);
+
+//Task 4 (switch/case)
+
+let score = Number(prompt("Please, input your score"));
+let resultScore;
+switch(true){
+    case (score >=0 && score < 60):
+        console.log("Your mark is F");
+    break;
+    case (score >=60 && score < 70):
+        console.log("Your mark is D");
+    break;
+    case (score >=70 && score < 80):
+        console.log("Your mark is C");
+    break;
+    case (score >=80 && score < 90):
+        console.log("Your mark is B");
+    break;
+    case (score >=90 && score <= 100):
+        console.log("Your mark is A");
+    break;
+    default: 
+        console.log("Lier!!! It's incorrect value");
+}
--- HomeWork2-2/index.js
+++ HomeWork2-2/index.js
@@ -3,8 +3,8 @@
 let a = 1;
 let b = 1;
 
-let c = ++a;
-let d = b++;
+let c = ++a; // Увеличиваем значение а и записываем в с
+let d = b++; // Записываем текущее значение b в d, при следующем вызове b увеличиваем его значение на 1
 
 console.log(a); // a = 2
 console.log(b); // b = 2
@@ -16,7 +16,8 @@ console.log(d); // d = 1
 let language = prompt("Enter the language").toLowerCase();
 
 switch(language){
-    case ("chinese" || "mandarin"):
+    case ("chinese"):
+    case ("mandarin"):
         console.log("MOST number of native speakers!");
     break;
     case "spanish":
@@ -32,6 +33,6 @@ switch(language){
         console.log("5th most spoken language");
     break;
     default:
-        console.log("Great language too :D")
+        console.log("Great language too :D");
         
 }
